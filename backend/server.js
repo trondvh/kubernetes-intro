e3ef7b35fd78cbdd930b36aba166b79ba906f7e6
trondvh <trond.hapnes@gmail.com>
2018-10-09 10:04:29 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,12 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/me', (request, response) => {
-	response.send('{"name": "Arthur Dent", "location": "Earth"}');
+	response.send('{"name": "Slartibarfast", "location": "Earth"}');
 	logger.info('Responded to GET request on /api/me')
 });
 
 app.get('/api/work', (request, response) => {
-	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
+	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Designer of planets"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
 	logger.info('Responded to GET request on /api/work')
 });
 
